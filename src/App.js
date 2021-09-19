bb66fb9de2ff330127069e957dcd50e78b2cf7fa
ViswaMithra16 <vmithravallabhaneni@gmail.com>
2021-09-18 21:03:20 -0700

    movieSearch_firstcommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import MovieComponent from './MovieResultComponent';
+import axios from 'axios';
+import MovieInfoComponent from './MovieInfoComponent';
+
+export const API_KEY ='813863c';
+
+const Container = styled.div`
+display:flex;
+flex-direction:column;
+`;
+
+const Header = styled.div`
+display:flex;
+flex-direction: row;
+background-color:black;
+color:white;
+padding:10px;
+font-size:30px;
+font-weight:bold;
+box-shadow:0 3px 6px 0 #555;
+justify-content: space-between;
+height:45px;
+align-items:center;
+`;
+
+const MovieIcon = styled.img`
+width: 50px;
+height: 48px;
+margin:15px;`;
+
+const AppName=styled.div`
+display:flex;
+flex-direction:row;
+align-items:center;
+`;
+
+const SearchBar = styled.div`
+display:flex;
+flex-direction:row;
+padding:10px 10px;
+background-color:white;
+border-radius:6px;
+margin-left:20px;
+width:50px;
+background-color:white;
+border-radius:6px;
+margin-left:20px;
+width:50%;
+background-color:white;
+align-items:center;
+`;
+
+const SearchIcon =styled.img`
+width:32px;
+height:32px;
+`;
+
+const SearchText =styled.input`
+color:black;
+font-size:16px;
+font-weight:regular;
+border:none;
+outline:none;
+margin-left:15px;
+`;
+
+const SearchResultsContainer =styled.div`
+display:flex;
+flex-direction:row;
+flex-wrap:wrap;
+padding:30px;
+justify-content:space-evenly;
+gap: 24px;
+`;
+
 
 function App() {
+
+  const [searchQuery, updateSearchQuery] = useState('arjun');
+  const [timeoutId, updateTimeoutId] = useState();
+  const [movieList, updateMovieList] = useState();
+  const [selectedMovie, onMovieSelect] = useState();
+
+  useEffect(() => {
+    initialLoad();
+  }, [])
+
+  const fetchData = async (searchString)=>{
+    const response = await axios.get(`https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`);
+    console.log(response);
+    updateMovieList(response.data.Search)
+  }
+
+  const onTextChange = (event)=>{
+    clearTimeout(timeoutId);
+    updateSearchQuery(event.target.value);
+    const timeout= setTimeout(()=>fetchData(event.target.value),500);
+    updateTimeoutId(timeout);
+  }
+
+  const initialLoad = () => {
+    clearTimeout(timeoutId);
+    const timeout= setTimeout(()=>fetchData(searchQuery),500);
+    updateTimeoutId(timeout);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Container>
+      <Header>
+            <AppName>
+              <MovieIcon src="/movie-icon.jpeg"/>
+              Movie Search
+            </AppName>
+            <SearchBar>
+              <SearchIcon src="/search-icon.png"/>
+              <SearchText placeholder="Search Movie" value={searchQuery} onChange={onTextChange}/>
+            </SearchBar>
+      </Header>
+      {selectedMovie && <MovieInfoComponent selectedMovie={selectedMovie} onMovieSelect={onMovieSelect}/>}
+      <SearchResultsContainer>
+        {movieList?.length?movieList.map((movie, index)=><MovieComponent key ={index} movie ={movie} 
+        onMovieSelect={onMovieSelect}/>):"No Movie Search"}
+      </SearchResultsContainer>
+   </Container>
   );
 }
 
---
+++ src/MovieResultComponent.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+const MovieContainer = styled.div`
+display:flex;
+flex-direction:column;
+padding:10px;
+width:  280px;
+box-shadow:0 3px 10px 0 #aaa;
+cursor:pointer;
+`;
+
+const MovieImage = styled.img`
+object-fit:cover;
+height:362px;
+`;
+
+const MovieName = styled.span`
+font-size:18px;
+font-weight:600;
+color:black;
+margin:15px 0;
+white-space:nowrap;
+text-overflow:ellipsis;
+overflow:hidden;
+`;
+
+const Info = styled.div`
+display:flex;
+flex-direction:row;
+justify-content:space-between;
+text-overflow: ellipsis;
+`;
+const MovieInfo = styled.span`
+font-size: 16px;
+font-weight: 500;
+color: black;
+white-space: nowrap;
+overflow:hidden;
+text-transform:capitalize;
+
+`;
+const MovieComponent =(props) =>{
+    const {Title, Year, imdbID, Type, Poster} = props.movie;
+
+    return (
+        <MovieContainer onClick={()=>{
+                props.onMovieSelect(imdbID)
+        }}>
+            <MovieImage src ={Poster}/>
+            <MovieName>{Title}</MovieName>
+            <Info>
+                <MovieInfo>Year: {Year}</MovieInfo>
+                <MovieInfo>Type: {Type}</MovieInfo>
+            </Info>
+        </MovieContainer>
+    );
+}
+
+export default MovieComponent;
